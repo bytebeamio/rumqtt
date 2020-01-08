6797c7829d9c4e81818aff95507bf9b8c9447d61
tekjar <kraviteza@gmail.com>
2020-01-08 21:57:13 +0530

    Add some tls code and increate connect packet timeout to 1 sec

--- rumq-broker/src/lib.rs
+++ rumq-broker/src/lib.rs
@@ -6,9 +6,19 @@ use tokio::net::TcpListener;
 use tokio::sync::mpsc::channel;
 use tokio::task;
 use tokio::time::{self, Elapsed};
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio_rustls::rustls::{ NoClientAuth, ServerConfig };
+use tokio_rustls::rustls::internal::pemfile::{ certs, rsa_private_keys };
+use tokio_rustls::rustls::TLSError;
+use tokio_rustls::TlsAcceptor;
 
-use std::io;
+use serde::Deserialize;
+
 use std::time::Duration;
+use std::sync::Arc;
+use std::io::{ self, BufReader };
+use std::fs::File;
+use std::path::Path;
 
 mod connection;
 mod graveyard;
@@ -21,69 +31,95 @@ pub enum Error {
     Mqtt(rumq_core::Error),
     Timeout(Elapsed),
     State(state::Error),
+    Tls(TLSError),
+    NoPrivateKey,
+    CertFail,
+    KeyFail,
     Disconnected,
 }
 
-/// Quotas and limitations mirroring google cloud
-/// TODO: Review all limitations and audit security aspects
-#[derive(Clone, Debug)]
-pub struct ConnectionConfig {
-    /// Max device id length
-    device_id_length:           usize,
-    /// Maximum payload size of telemetry event
-    telemetry_payload_size:     usize,
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub port: u16,
+    pub connection_timeout_ms: u16,
+    pub max_client_id_len: usize,
+    pub max_connections: usize,
     /// Throughput from cloud to device
-    cloud_to_device_throughput: usize,
+    pub max_cloud_to_device_throughput: usize,
     /// Throughput from device to cloud
-    device_to_cloud_throughput: usize,
+    pub max_device_to_cloud_throughput: usize,
     /// Minimum delay time between consecutive outgoing packets
-    incoming_messages_per_sec:  usize,
-    /// maximum size of all inflight messages
-    inflight_size:              usize,
+    pub max_incoming_messages_per_sec:  usize,
+    pub disk_persistence: bool,
+    pub disk_retention_size: usize,
+    pub disk_retention_time_sec: usize,
+    pub auto_save_interval_sec: u16,
+    pub max_packet_size: usize,
+    pub max_inflight_queue_size: usize,
+    pub ca_path: Option<String>,
+    pub cert_path: Option<String>,
+    pub key_path: Option<String>,
+    pub client_auth: bool,
+    pub username: Option<String>,
+    pub password: Option<String>,
 }
 
-impl Default for ConnectionConfig {
-    fn default() -> Self {
-        ConnectionConfig {
-            device_id_length:           256,
-            // 100KB per event
-            telemetry_payload_size:     100 * 1024,
-            // 100 KB/s throughput
-            cloud_to_device_throughput: 100 * 1024,
-            // 100 KB/s throughput
-            device_to_cloud_throughput: 100 * 1024,
-            // 10ms constant delay between each event
-            incoming_messages_per_sec:  100,
-            // 10MB
-            inflight_size:              10 * 1024 * 1024,
-        }
-    }
+async fn tls_connection<P: AsRef<Path>>(ca_path: P, key_path: P) -> Result<TlsAcceptor, Error> {
+    let mut server_config = ServerConfig::new(NoClientAuth::new());
+
+    let certs = certs(&mut BufReader::new(File::open(ca_path)?)).map_err(|_|Error::CertFail)?;
+    let mut keys = rsa_private_keys(&mut BufReader::new(File::open(key_path)?)).map_err(|_| Error::KeyFail)?;
+
+    server_config.set_single_cert(certs, keys.remove(0))?;
+    let acceptor = TlsAcceptor::from(Arc::new(server_config));
+    Ok(acceptor)
 }
 
-pub async fn accept_loop(addr: &str) -> Result<(), Error> {
-    let connection_config = ConnectionConfig::default();
-    let mut listener = TcpListener::bind(addr).await?;
+pub async fn accept_loop(config: Arc<Config>, addr: &str) -> Result<(), Error> {
+    let router_config = config.clone();
+    let connection_config = config.clone();
     let (router_tx, router_rx) = channel::<router::RouterMessage>(10);
-
-    // graveyard to save state of persistent connections
     let graveyard = graveyard::Graveyard::new();
 
     // router to route data between connections. creates an extra copy but
     // might not be a big deal if we prevent clones/send fat pointers and batch
     task::spawn(async move {
-        let mut router = router::Router::new(router_rx);
+        let mut router = router::Router::new(router_config, router_rx);
         router.start().await
     });
 
+    let acceptor = if let Some(ca_path) = config.ca_path.as_ref() {
+        let key_path = config.key_path.as_ref().ok_or(Error::NoPrivateKey)?;
+        Some(tls_connection(ca_path, key_path).await?)
+    } else {
+        None
+    };
+
     info!("Waiting for connection");
     // eventloop which accepts connections
+    let mut listener = TcpListener::bind(addr).await?;
     loop {
         let (stream, addr) = listener.accept().await?;
         info!("Accepting from: {}", addr);
-        task::spawn(connection::eventloop(connection_config.clone(), graveyard.clone(), stream, router_tx.clone()));
+
+        let config = connection_config.clone(); 
+        let graveyard = graveyard.clone();
+        let router_tx = router_tx.clone();
+
+        if let Some(acceptor) = &acceptor {
+            let stream = acceptor.accept(stream).await?;
+            task::spawn(connection::eventloop(config, graveyard, stream, router_tx));
+        } else {
+            task::spawn(connection::eventloop(config, graveyard, stream, router_tx));
+        };
+
+
         time::delay_for(Duration::from_millis(1)).await;
     }
 }
+
+pub trait Network: AsyncWrite + AsyncRead + Unpin + Send {}
+impl<T> Network for T where T: AsyncWrite + AsyncRead + Unpin + Send {}
 
 #[cfg(test)]
 mod test {
--- rumq-broker/src/rumqd.rs
+++ rumq-broker/src/rumqd.rs
@@ -1,12 +1,30 @@
 use tokio::task;
+use structopt::StructOpt;
 
-use librumqd::accept_loop;
+use std::fs;
+use std::sync::Arc;
+use std::path::PathBuf;
+
+use librumqd::{accept_loop, Config};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Rumqd", about = "High performance asynchronous mqtt broker")]
+pub struct CommandLine {
+    #[structopt(short = "c", help = "Rumqd config file", default_value = "rumqd.conf", parse(from_os_str))]
+    config_path: PathBuf,
+}
+
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let fut = accept_loop("0.0.0.0:1883");
+    let commandline = CommandLine::from_args();
+    let config = fs::read_to_string(commandline.config_path).unwrap();
+    let config = toml::from_str::<Config>(&config).unwrap();
+    let config = Arc::new(config);
+    
+    let fut = accept_loop(config, "0.0.0.0:1883");
     let o = task::spawn(fut).await;
 
     println!("{:?}", o);
