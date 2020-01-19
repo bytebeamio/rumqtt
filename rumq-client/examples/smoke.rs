fd3d131deeff9121dba14c6af35fa141f11d2712
tekjar <kraviteza@gmail.com>
2020-01-19 18:39:33 +0530

    Remove thread usage from example

--- rumq-client/examples/smoke.rs
+++ rumq-client/examples/smoke.rs
@@ -1,5 +1,4 @@
 use futures_util::stream::StreamExt;
-use std::thread;
 use tokio::sync::mpsc::{channel, Sender};
 use tokio::task;
 use tokio::time;
@@ -14,14 +13,12 @@ async fn main() {
 
     let (requests_tx, requests_rx) = channel(10);
     let mut mqttoptions = MqttOptions::new("test-1", "localhost", 1883);
-    mqttoptions.set_keep_alive(5).set_throttle(Duration::from_secs(1));
+    mqttoptions.set_keep_alive(30).set_throttle(Duration::from_secs(1));
     let mut eventloop = eventloop(mqttoptions, requests_rx);
 
-    thread::spawn(move || {
-        requests(requests_tx);
-        thread::sleep(Duration::from_secs(3));
+    task::spawn(async move {
+        requests(requests_tx).await;
     });
-
 
     stream_it(&mut eventloop).await;
 }
@@ -37,16 +34,13 @@ async fn stream_it(eventloop: &mut MqttEventLoop) {
     println!("Stream done");
 }
 
-#[tokio::main(basic_scheduler)]
 async fn requests(mut requests_tx: Sender<Request>) {
-    task::spawn(async move {
-        for i in 0..10 {
-            requests_tx.send(publish_request(i)).await.unwrap();
-            time::delay_for(Duration::from_secs(1)).await; 
-        }
+    for i in 0..10 {
+        requests_tx.send(publish_request(i)).await.unwrap();
+        time::delay_for(Duration::from_secs(1)).await; 
+    }
 
-        time::delay_for(Duration::from_secs(30)).await; 
-    }).await.unwrap();
+    time::delay_for(Duration::from_secs(600)).await; 
 }
 
 fn publish_request(i: u8) -> Request {
