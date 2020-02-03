4353edac69212a44d82c41fbcc98605d4e9639a1
tekjar <kraviteza@gmail.com>
2020-02-03 16:52:15 +0530

    Update benchmarking tool

--- utils/benchmark/benchmark.go
+++ utils/benchmark/benchmark.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"sync/atomic"
 	"time"
@@ -11,70 +12,118 @@ import (
 
 
 var counter uint64
-var start = time.Now()
 var end = time.Now()
 
 const totalPublishes = 100000
 const payloadSize= 1024
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 func data(n int) string {
-    b := make([]byte, n)
-    for i := range b {
-        b[i] = letterBytes[rand.Intn(len(letterBytes))]
-    }
-    
-    return string(b)
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+
+	return string(b)
 }
 
-func main() {
-	exit := make(chan bool, totalPublishes)
+type Connection struct {
+	id string
+	total uint64
+	client mqtt.Client
+	stats chan uint64
+}
 
+func NewConnection(id string, total uint64, stats chan uint64) *Connection {
 	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883")
-	opts.SetClientID("mqtt-benchmark")
+	opts.SetClientID(id)
+	opts.SetProtocolVersion(4)
+	opts.SetCleanSession(true)
+	opts.SetKeepAlive(10 * time.Second)
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		fmt.Println("Error = ", token.Error())
+		os.Exit(1)
+	}
+
+	return &Connection {
+		id: id,
+		client: c,
+		total: total,
+		stats: stats,
+	}
+}
+
+func (c *Connection) Start() {
+	var counter uint64
+	var start = time.Now()
 
 	msgHandler := func(client mqtt.Client, msg mqtt.Message) {
-		c := atomic.AddUint64(&counter, 1)
+		count := atomic.AddUint64(&counter, 1)
 
-		if c >= totalPublishes {
-			exit <- true
+		if count == c.total {
+			throughput := (totalPublishes * payloadSize)/1024.0/1024.0/time.Since(start).Seconds()
+			fmt.Println("Id = ", c.id, "Throughput = ", throughput,  "MB/s")
+			c.stats <- uint64(throughput)
 		}
 	}
 
-
-	//create and start a client using the above ClientOptions
-	c := mqtt.NewClient(opts)
-
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-
-	if token := c.Subscribe("hello/mqtt/rumqtt", 1, msgHandler); token.Wait() && token.Error() != nil {
-		panic(token.Error())
+	if token := c.client.Subscribe("hello/mqtt/rumqtt", 1, msgHandler); token.Wait() && token.Error() != nil {
+		fmt.Println("Error = ", token.Error())
+		os.Exit(1)
 	}
 
 	go func() {
 		text := data(payloadSize)
 		for i := 0; i < totalPublishes; i++ {
-			token := c.Publish("hello/mqtt/rumqtt", 1, false, text)
+			token := c.client.Publish("hello/mqtt/rumqtt", 1, false, text)
 			token.Wait()
 		}
-
-		fmt.Println("Throughput = ", (totalPublishes * payloadSize)/1024.0/1024.0/time.Since(start).Seconds(), "MB/s")
 	}()
+	
+	for {
+		<-time.After(100 * time.Second)
+		fmt.Println("incoming pub count = ", atomic.LoadUint64(&counter), ". time taken for incoming pubs = ",  time.Since(start))
+	}
+}
 
-L:
+func main() {
+	exit := make(chan uint64, 1)
+	totalConnections := 10
+	totalDone := 0
+	throughputs := make([]uint64, 0)
+	// var start = time.Now()
+
+	for i := 0; i < totalConnections; i++ {
+		time.Sleep(10 * time.Millisecond)
+		id := fmt.Sprintf("bench-%v", i)
+		connection := NewConnection(id, 10000, exit)
+		go connection.Start()
+	}
+
+	L:
 	for {
 		select {
-		case e := <-exit:
-			if e {
+		case throughput := <-exit:
+			totalDone += 1
+			throughputs = append(throughputs, throughput)
+			if totalDone >= totalConnections {
 				break L
 			}
 		case <-time.After(5 * time.Second):
-			fmt.Println("incoming pub count = ", atomic.LoadUint64(&counter), ". time taken for incoming pubs = ",  time.Since(start))
+			// fmt.Println("incoming pub count = ", atomic.LoadUint64(&counter), ". time taken for incoming pubs = ",  time.Since(start))
 		}
 	}
 
-	fmt.Println("incoming pub count = ", atomic.LoadUint64(&counter), ". time taken for incoming pubs = ",  time.Since(start))
-	time.Sleep(1 * time.Second)
+
+	count := len(throughputs)
+	var total uint64 = 0
+	for i := 0; i < count; i++ {
+		total += throughputs[i]
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Average throughput = ", float64(total)/float64(count), "MB/s")
 }
--- utils/benchmark/go.mod
+++ utils/benchmark/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
 )
