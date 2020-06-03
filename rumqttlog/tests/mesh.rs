ffb9e3bb13382506041808bef1c9a8ad49d93e71
tekjar <raviteja@bytebeam.io>
2020-06-03 15:25:47 +0530

    Add more methods

--- rumqttlog/tests/mesh.rs
+++ rumqttlog/tests/mesh.rs
@@ -6,7 +6,7 @@ use bytes::Bytes;
 use std::path::PathBuf;
 use std::thread;
 use tokio::time::Duration;
-use rumqttlog::{Config, Router, RouterInMessage, Sender, Connection, RouterOutMessage, bounded, Receiver};
+use rumqttlog::{Config, Router, RouterInMessage, Sender, Connection, RouterOutMessage, bounded, Receiver, DataReply, DataRequest};
 use rumqttlog::router::{Data, ConnectionAck};
 use tokio::task;
 
@@ -29,8 +29,6 @@ fn three_node_mesh() {
 // TODO not workin with core_threads = 1. Investigate
 #[tokio::main(core_threads = 2)]
 async fn node_1() {
-    // pretty_env_logger::init();
-
     let config = config(1);
     let (router, router_tx) = Router::new(config);
 
@@ -39,9 +37,8 @@ async fn node_1() {
         router.start().await
     });
 
+    // Register, send data and receive acks
     let (id, this_rx) = register_with_router(100, "connection-1", &router_tx).await;
-
-    // incoming data from the router
     task::spawn(async move {
         for i in 1..=COUNT {
             receive_ack(&this_rx, i).await;
@@ -53,6 +50,29 @@ async fn node_1() {
     }
 }
 
+#[tokio::main(core_threads = 2)]
+async fn node_2() {
+    let config = config(2);
+    let (router, router_tx) = Router::new(config);
+
+    task::spawn(async move {
+        let mut router = router;
+        router.start().await
+    });
+
+    let (id, this_rx) = register_with_router(100, "connection-2", &router_tx).await;
+    // incoming data from the router
+    loop {
+        let reply = wait_for_new_data(&this_rx).await;
+    }
+
+}
+
+
+/// Everything from here is a list of helper methods to make the test readable
+/// ---------------------------------------------------------------------------
+///
+
 async fn receive_ack(this_rx: &Receiver<RouterOutMessage>, pkid: u64) {
     match this_rx.recv().await.unwrap() {
         RouterOutMessage::DataAck(ack) => {
@@ -61,6 +81,36 @@ async fn receive_ack(this_rx: &Receiver<RouterOutMessage>, pkid: u64) {
         },
         message => panic!("Invalid message = {:?}", message)
     };
+}
+
+async fn wait_for_new_data(rx: &Receiver<RouterOutMessage>) -> DataReply {
+    tokio::time::delay_for(Duration::from_secs(1)).await;
+    match rx.try_recv() {
+        Ok(RouterOutMessage::DataReply(reply)) => reply,
+        v => panic!("Expecting Data Reply. {:?}", v)
+    }
+}
+
+fn ask_data(
+    id: usize,
+    router_tx: &Sender<(ConnectionId, RouterInMessage)>,
+    topic: &str,
+    native_offset: u64,
+    replica_offset: u64,
+) {
+    let message = (
+        id,
+        RouterInMessage::DataRequest(DataRequest {
+            topic: topic.to_string(),
+            native_segment: 0,
+            replica_segment: 0,
+            native_offset,
+            replica_offset,
+            size: 100 * 1024
+        }),
+    );
+
+    router_tx.try_send(message).unwrap();
 }
 
 async fn send_data(
