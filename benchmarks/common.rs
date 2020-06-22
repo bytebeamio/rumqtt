80ddea0f4552c0243102e2dffde4bd9d7922823d
tekjar <raviteja@bytebeam.io>
2020-06-22 15:04:46 +0530

    Some basic mesh code

--- benchmarks/common.rs
+++ benchmarks/common.rs
@@ -2,7 +2,10 @@ use pprof::ProfilerGuard;
 use prost::Message;
 use std::fs::File;
 use std::io::Write;
+use rumqttlog::{Sender, RouterInMessage, Receiver, RouterOutMessage, Connection};
+use rumqttlog::router::ConnectionAck;
 
+#[allow(unused)]
 pub fn profile(name: &str, guard: ProfilerGuard) {
     if let Ok(report) = guard.report().build() {
         let mut file = File::create(name).unwrap();
@@ -12,4 +15,27 @@ pub fn profile(name: &str, guard: ProfilerGuard) {
         profile.encode( & mut content).unwrap();
         file.write_all( & content).unwrap();
     };
+}
+
+pub async fn new_connection(
+    id: &str,
+    cap: usize,
+    router_tx: &Sender<(usize, RouterInMessage)>
+) -> (usize, Receiver<RouterOutMessage>) {
+    let (connection, this_rx) = Connection::new(id, cap);
+
+    // send a connection request with a dummy id
+    let message = (0, RouterInMessage::Connect(connection));
+    router_tx.send(message).await.unwrap();
+
+    // wait for ack from router
+    let id = match this_rx.recv().await.unwrap() {
+        RouterOutMessage::ConnectionAck(ConnectionAck::Success(id)) => id,
+        RouterOutMessage::ConnectionAck(ConnectionAck::Failure(e)) => {
+            panic!("Connection failed {:?}", e)
+        }
+        message => panic!("Not connection ack = {:?}", message),
+    };
+
+    (id, this_rx)
 }
