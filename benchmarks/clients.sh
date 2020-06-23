f596ee1cafcee45d5faefc8b6b5fb26911c780bd
tekjar <raviteja@bytebeam.io>
2020-06-23 09:22:48 +0530

    Add paho async

--- benchmarks/clients.sh
+++ benchmarks/clients.sh
@@ -3,5 +3,6 @@ set -e
 
 cargo run -q --bin rumqttasync --release
 cargo run -q --bin rumqttsync --release
-cargo run -q --bin paho --release
+cargo run -q --bin pahoasync --release
+#cargo run -q --bin pahosync --release
 go run paho.go
