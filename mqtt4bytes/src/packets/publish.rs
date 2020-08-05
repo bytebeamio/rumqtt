bytebeamio/rumqtt

commit e6642d8a8e852c8cbd2267ab83b4fd4d92de2e72

RawPublish (#99)
* Fix batched requests and use it in benchmarks

* Use jemalloc

* Fix duplicate responses due to strays in old connetion

* Serialize publishes at source

Co-authored-by: tekjar <raviteja@bytebeam.io>

mqtt4bytes/src/packets/publish.rs
@@ -28,6 +28,10 @@ impl Publish {
         }
     }
 
+    pub fn raw(self) -> Result<PublishRaw, Error> {
+        PublishRaw::from_bytes(self.topic, self.qos, self.payload)
+    }
+
     pub fn from_bytes<S: Into<String>>(topic: S, qos: QoS, payload: Bytes) -> Publish {
         Publish {
             dup: false,
@@ -114,6 +118,7 @@ impl Publish {
         Ok(1 + remaining_len_bytes + remaining_len)
     }
 }
+
 
 impl fmt::Debug for Publish {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {

benchmarks/Cargo.toml
@@ -20,6 +20,7 @@ async-channel = "1"
 pretty_env_logger = "0.4"
 prost = "0.6"
 pprof = { version = "0.3", features = ["flamegraph", "protobuf"] }
+jemallocator = "0.3"
 
 [[bin]]
 name = "rumqttasync"

benchmarks/rumqttasync.rs
@@ -1,4 +1,4 @@
-use rumqttc::{self, EventLoop, MqttOptions, Incoming, QoS, Request, Publish};
+use rumqttc::{self, EventLoop, MqttOptions, Incoming, QoS, Request, PublishRaw};
 use std::time::{Duration, Instant};
 use std::error::Error;
 
@@ -8,11 +8,14 @@ use async_channel::Sender;
 
 mod common;
 
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 #[tokio::main(core_threads = 2)]
 async fn main() {
-    pretty_env_logger::init();
+    // pretty_env_logger::init();
     // let guard = pprof::ProfilerGuard::new(100).unwrap();
-    start("rumqtt-async", 100, 1_000_00).await.unwrap();
+    start("rumqtt-async", 100, 1_000_000).await.unwrap();
     // common::profile("bench.pb", guard);
 }
 
@@ -21,7 +24,8 @@ pub async fn start(id: &str, payload_size: usize, count: usize) -> Result<() , B
     mqttoptions.set_keep_alive(20);
 
     // NOTE More the inflight size, better the perf
-    mqttoptions.set_inflight(100);
+    mqttoptions.set_inflight(1000);
+    mqttoptions.set_max_request_batch(10);
 
     let mut eventloop = EventLoop::new(mqttoptions, 10).await;
     let requests_tx = eventloop.handle();
@@ -72,8 +76,8 @@ async fn requests(id: &str, payloads: Vec<Vec<u8>>, requests_tx: Sender<Request>
     // let subscription = rumqttc::Subscribe::new(&topic, QoS::AtLeastOnce);
     // let _ = requests_tx.send(Request::Subscribe(subscription)).await;
     for payload in payloads.into_iter() {
-        let publish = Publish::new(&topic, QoS::AtLeastOnce, payload);
-        let publish = Request::Publish(publish);
+        let publish = PublishRaw::new(&topic, QoS::AtLeastOnce, payload).unwrap();
+        let publish = Request::PublishRaw(publish);
         if let Err(_) = requests_tx.send(publish).await {
             break;
         }

benchmarks/rumqttasyncqos0.rs
@@ -1,4 +1,4 @@
-use rumqttc::{self, EventLoop, MqttOptions, Incoming, QoS, Request, Publish};
+use rumqttc::{self, EventLoop, MqttOptions, Incoming, QoS, Request, PublishRaw};
 use std::time::{Duration, Instant};
 use std::error::Error;
 
@@ -7,6 +7,9 @@ use tokio::time;
 use async_channel::Sender;
 
 mod common;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[tokio::main(core_threads = 2)]
 async fn main() {
@@ -19,6 +22,7 @@ async fn main() {
 pub async fn start(id: &str, payload_size: usize, count: usize) -> Result<() , Box<dyn Error>> {
     let mut mqttoptions = MqttOptions::new(id, "localhost", 1883);
     mqttoptions.set_keep_alive(1000);
+    mqttoptions.set_max_request_batch(10);
 
     // NOTE More the inflight size, better the perf
     mqttoptions.set_inflight(100);
@@ -65,8 +69,8 @@ async fn requests(id: &str, payloads: Vec<Vec<u8>>, requests_tx: Sender<Request>
     // let subscription = rumqttc::Subscribe::new(&topic, QoS::AtLeastOnce);
     // let _ = requests_tx.send(Request::Subscribe(subscription)).await;
     for payload in payloads.into_iter() {
-        let publish = Publish::new(&topic, QoS::AtMostOnce, payload);
-        let publish = Request::Publish(publish);
+        let publish = PublishRaw::new(&topic, QoS::AtMostOnce, payload).unwrap();
+        let publish = Request::PublishRaw(publish);
         requests_tx.send(publish).await.unwrap();
     }
 

benchmarks/rumqttsync.rs
@@ -5,6 +5,10 @@ use std::thread;
 
 mod common;
 
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 fn main() {
     pretty_env_logger::init();
     let guard = pprof::ProfilerGuard::new(250).unwrap();
@@ -15,6 +19,7 @@ fn main() {
 pub fn start(id: &str, payload_size: usize, count: usize) -> Result<() , Box<dyn Error>> {
     let mut mqttoptions = MqttOptions::new(id, "localhost", 1883);
     mqttoptions.set_keep_alive(20);
+    mqttoptions.set_max_request_batch(10);
 
     // NOTE More the inflight size, better the perf
     mqttoptions.set_inflight(100);

mqtt4bytes/src/packets/mod.rs
@@ -2,6 +2,7 @@ mod ping;
 mod connect;
 mod connack;
 mod publish;
+mod publishraw;
 mod puback;
 mod pubrec;
 mod pubrel;
@@ -15,6 +16,7 @@ mod disconnect;
 pub use connect::*;
 pub use connack::*;
 pub use publish::*;
+pub use publishraw::*;
 pub use puback::*;
 pub use pubrec::*;
 pub use pubrel::*;

mqtt4bytes/src/packets/publishraw.rs
@@ -1,0 +1,120 @@
+use bytes::{BytesMut, BufMut};
+use alloc::vec::Vec;
+use alloc::string::String;
+use crate::*;
+use super::*;
+
+#[derive(Clone, PartialEq)]
+pub struct PublishRaw {
+    pub header: BytesMut,
+    pub qos: QoS,
+    pub pkid: u16,
+    pub payload: Bytes
+}
+
+impl PublishRaw {
+    pub fn new<S: Into<String>, P: Into<Vec<u8>>>(topic: S, qos: QoS, payload: P) -> Result<PublishRaw, Error> {
+        PublishRaw::from_bytes(topic, qos, Bytes::from(payload.into()))
+    }
+
+    pub fn from_bytes<S: Into<String>>(topic: S, qos: QoS, payload: Bytes) -> Result<PublishRaw, Error> {
+        let dup = false as u8;
+        let qos_raw = qos as u8;
+        let retain = false as u8;
+        let topic = topic.into();
+
+        // for publish, variable header = 2 + topic + packet id (optional) + payload
+        let mut remaining_len = topic.len() + 2 + payload.len();
+        if qos != QoS::AtMostOnce {
+            remaining_len += 2;
+        }
+
+        let mut header = BytesMut::with_capacity(4 + remaining_len);
+        header.put_u8(0b0011_0000 | retain | qos_raw << 1 | dup << 3);
+        write_remaining_length(&mut header, remaining_len)?;
+
+        write_mqtt_string(&mut header, topic.as_str());
+        if qos != QoS::AtMostOnce {
+            header.put_u16(0);
+        }
+
+        Ok(PublishRaw {
+            header,
+            qos,
+            pkid: 0,
+            payload: Bytes::from(payload)
+        })
+    }
+
+    pub fn set_pkid(&mut self, pkid: u16) -> &mut Self {
+        if self.qos != QoS::AtMostOnce {
+            self.pkid = pkid;
+            let len = self.header.len();
+            self.header.truncate(len - 2);
+            self.header.put_u16(pkid);
+        }
+
+        self
+    }
+
+    pub fn set_retain(&mut self, retain: bool) -> &mut Self {
+        self.header[0] |= retain as u8;
+        self
+    }
+
+    pub fn set_dup(&mut self, dup: bool) -> &mut Self {
+        self.header[0] |= (dup as u8) << 3;
+        self
+    }
+
+
+    pub fn write(&self, payload: &mut BytesMut) -> Result<usize, Error> {
+        let len = self.header.len() + self.payload.len();
+        payload.extend_from_slice(&self.header);
+        payload.extend_from_slice(&self.payload);
+        Ok(len)
+    }
+}
+
+
+
+impl fmt::Debug for PublishRaw {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "raw publish")
+
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use alloc::vec;
+
+    #[test]
+    fn write_packet_publish_at_least_once_works() {
+        let mut publish = PublishRaw::new("a/b", QoS::AtLeastOnce, vec![0xF1, 0xF2, 0xF3, 0xF4]).unwrap();
+        publish.set_pkid(10);
+
+        let mut buf = BytesMut::new();
+        publish.write(&mut buf).unwrap();
+
+        assert_eq!(
+            buf,
+            vec![
+                0b0011_0010,
+                11,
+                0x00,
+                0x03,
+                b'a',
+                b'/',
+                b'b',
+                0x00,
+                0x0a,
+                0xF1,
+                0xF2,
+                0xF3,
+                0xF4
+            ]
+        );
+    }
+}

rumqttc/src/client.rs
@@ -17,6 +17,8 @@ pub enum ClientError {
     Cancel(#[from] SendError<()>),
     #[error("Failed to send mqtt requests to eventloop")]
     Request(#[from] SendError<Request>),
+    #[error("Serialization error")]
+    Mqtt4(mqtt4bytes::Error),
 }
 
 /// `Client` to communicate with MQTT eventloop `Connection`.
@@ -69,10 +71,15 @@ impl Client {
         S: Into<String>,
         V: Into<Vec<u8>>,
     {
-        let mut publish = Publish::new(topic, qos, payload.into());
-        publish.retain = retain;
-        let request = Request::Publish(publish);
-        blocking::block_on(self.request_tx.send(request))?;
+
+        let mut publish = match PublishRaw::new(topic, qos, payload) {
+            Ok(publish) => publish,
+            Err(e) => return Err(ClientError::Mqtt4(e))
+        };
+
+        publish.set_retain(retain);
+        let publish = Request::PublishRaw(publish);
+        blocking::block_on(self.request_tx.send(publish))?;
         Ok(())
     }
 

commit d3ef4e1b421e65c2e49ea37433ba1ae8683e8010

Don't lose pkid, dup and retain when converting Publish to PublishRaw.
This fixes bug #105, which was introduced by pull #99.

mqtt4bytes/src/packets/publish.rs
@@ -29,7 +29,16 @@ impl Publish {
     }
 
     pub fn raw(self) -> Result<PublishRaw, Error> {
-        PublishRaw::from_bytes(self.topic, self.qos, self.payload)
+        let pkid = self.pkid;
+        let dup = self.dup;
+        let retain = self.retain;
+        PublishRaw::from_bytes(self.topic, self.qos, self.payload).map(|mut raw|
+        {
+            raw.set_pkid(pkid);
+            raw.set_dup(dup);
+            raw.set_retain(retain);
+            raw
+        })
     }
 
     pub fn from_bytes<S: Into<String>>(topic: S, qos: QoS, payload: Bytes) -> Publish {