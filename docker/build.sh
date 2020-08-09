6bae176d1f85f65373a008ec3f01f98cefb247dd
tekjar <raviteja@bytebeam.io>
2020-08-10 00:22:05 +0530

    Add job dependency and fix copy in docker build

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-cp -r ../target/release/rumqttd target
-cp -r ../rumqttd/config target
+docker stop rumqttd || true
+
+cp -r ../target/release/rumqttd target/
+cp -r ../rumqttd/config target/
 
 docker build -t rumqttd .
