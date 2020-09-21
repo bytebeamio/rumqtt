d99e49db581faf296b6f0ce1e6384523b61ca5eb
tekjar <raviteja@bytebeam.io>
2020-09-21 22:59:18 +0530

    Improve benchmarks

--- benchmarks/rumqttsync.rs
+++ benchmarks/rumqttsync.rs
@@ -10,23 +10,26 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 fn main() {
     pretty_env_logger::init();
-    let guard = pprof::ProfilerGuard::new(250).unwrap();
+    let guard = pprof::ProfilerGuard::new(100).unwrap();
     start("rumqtt-sync", 100, 1_000_000).unwrap();
     common::profile("bench.pb", guard);
 }
 
+
 pub fn start(id: &str, payload_size: usize, count: usize) -> Result<(), Box<dyn Error>> {
     let mut mqttoptions = MqttOptions::new(id, "localhost", 1883);
     mqttoptions.set_keep_alive(20);
+    mqttoptions.set_inflight(100);
     mqttoptions.set_max_request_batch(10);
 
-    // NOTE More the inflight size, better the perf
-    mqttoptions.set_inflight(100);
-
-    let (client, mut connection) = Client::new(mqttoptions, 10);
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
     thread::spawn(move || {
-        let mut client = client;
-        requests(count, payload_size, &mut client);
+        for _i in 0..count {
+            let payload = vec![0; payload_size];
+            let qos = QoS::AtLeastOnce;
+            client.publish("hello/benchmarks/world", qos, false, payload).unwrap();
+        }
+
         thread::sleep(Duration::from_secs(1));
     });
 
@@ -43,21 +46,12 @@ pub fn start(id: &str, payload_size: usize, count: usize) -> Result<(), Box<dyn 
     }
 
     let elapsed_ms = start.elapsed().as_millis();
-    let throughput = (acks_count as usize * 1000) / elapsed_ms as usize;
+    let throughput = acks_count as usize / elapsed_ms as usize;
+    let throughput = throughput * 1000;
     println!(
         "Id = {}, Messages = {}, Payload (bytes) = {}, Throughput (messages/sec) = {}",
-        id, acks_count, payload_size, throughput,
+        id, count, payload_size, throughput,
     );
-    Ok(())
-}
 
-fn requests(count: usize, payload_size: usize, client: &mut Client) {
-    for i in 0..count {
-        let mut payload = vec![1; payload_size];
-        payload[0] = (i % 255) as u8;
-        if let Err(e) = client.publish("hello/world", QoS::AtLeastOnce, false, payload) {
-            println!("Client error: {:?}", e);
-            break;
-        }
-    }
+    Ok(())
 }
