d484cc31445f3006fd6e12197d692e5e49fec4d5
Jonas Platte <jplatte+git@posteo.de>
2020-09-29 19:49:33 +0200

    Remove needless fn main() in doctest

--- rumqttc/src/lib.rs
+++ rumqttc/src/lib.rs
@@ -9,25 +9,23 @@
 //! ----------------------------
 //!
 //! ```no_run
-//!use rumqttc::{MqttOptions, Client, QoS};
-//!use std::time::Duration;
-//!use std::thread;
+//! use rumqttc::{MqttOptions, Client, QoS};
+//! use std::time::Duration;
+//! use std::thread;
 //!
-//! fn main() {
-//!     let mut mqttoptions = MqttOptions::new("rumqtt-sync", "test.mosquitto.org", 1883);
-//!     mqttoptions.set_keep_alive(5);
+//! let mut mqttoptions = MqttOptions::new("rumqtt-sync", "test.mosquitto.org", 1883);
+//! mqttoptions.set_keep_alive(5);
 //!
-//!     let (mut client, mut connection) = Client::new(mqttoptions, 10);
-//!     client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
-//!     thread::spawn(move || for i in 0..10 {
-//!        client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).unwrap();
-//!        thread::sleep(Duration::from_millis(100));
-//!     });
+//! let (mut client, mut connection) = Client::new(mqttoptions, 10);
+//! client.subscribe("hello/rumqtt", QoS::AtMostOnce).unwrap();
+//! thread::spawn(move || for i in 0..10 {
+//!    client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).unwrap();
+//!    thread::sleep(Duration::from_millis(100));
+//! });
 //!
-//!     // Iterate to poll the eventloop for connection progress
-//!     for (i, notification) in connection.iter().enumerate() {
-//!         println!("Notification = {:?}", notification);
-//!     }
+//! // Iterate to poll the eventloop for connection progress
+//! for (i, notification) in connection.iter().enumerate() {
+//!     println!("Notification = {:?}", notification);
 //! }
 //! ```
 //! A simple asynchronous publish and subscribe
