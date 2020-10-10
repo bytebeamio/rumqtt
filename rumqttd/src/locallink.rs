03976b03c9acc29d93f5f6a630809fc7deb5e66e
tekjar <raviteja@bytebeam.io>
2020-10-10 22:02:40 +0530

    Fix compilation

--- rumqttd/src/locallink.rs
+++ rumqttd/src/locallink.rs
@@ -1,30 +1,30 @@
 use crate::Id;
 use mqtt4bytes::{Packet, Publish, QoS, Subscribe};
 use rumqttlog::{
-    tracker::Tracker, Connection, ConnectionAck, DataReply, Receiver, RecvError, RouterInMessage,
-    RouterOutMessage, SendError, Sender,
+    Connection, ConnectionAck, Data, Receiver, RecvError, Event,
+    Notification, SendError, Sender,
 };
 
 #[derive(Debug, thiserror::Error)]
 pub enum LinkError {
     #[error("Unexpected router message")]
-    NotConnectionAck(RouterOutMessage),
+    NotConnectionAck(Notification),
     #[error("Connack error {0}")]
     ConnectionAck(String),
     #[error("Channel send error")]
-    Send(#[from] SendError<(Id, RouterInMessage)>),
+    Send(#[from] SendError<(Id, Event)>),
     #[error("Channel recv error")]
     Recv(#[from] RecvError),
 }
 
 pub struct LinkTx {
     id: usize,
-    router_tx: Sender<(Id, RouterInMessage)>,
+    router_tx: Sender<(Id, Event)>,
     client_id: String,
 }
 
 impl LinkTx {
-    pub(crate) fn new(client_id: &str, router_tx: Sender<(Id, RouterInMessage)>) -> LinkTx {
+    pub(crate) fn new(client_id: &str, router_tx: Sender<(Id, Event)>) -> LinkTx {
         LinkTx {
             id: 0,
             router_tx,
@@ -36,13 +36,13 @@ impl LinkTx {
         // connection queue capacity should match that maximum inflight requests
         let (connection, link_rx) = Connection::new_remote(&self.client_id, max_inflight_requests);
 
-        let message = (0, RouterInMessage::Connect(connection));
+        let message = (0, Event::Connect(connection));
         self.router_tx.send(message).unwrap();
 
         // Right now link identifies failure with dropped rx in router, which is probably ok
         // We need this here to get id assigned by router
         match link_rx.recv()? {
-            RouterOutMessage::ConnectionAck(ack) => match ack {
+            Notification::ConnectionAck(ack) => match ack {
                 ConnectionAck::Success(id) => self.id = id,
                 ConnectionAck::Failure(reason) => return Err(LinkError::ConnectionAck(reason)),
             },
@@ -68,7 +68,7 @@ impl LinkTx {
     {
         let mut publish = Publish::new(topic, QoS::AtLeastOnce, payload);
         publish.retain = retain;
-        let message = RouterInMessage::Publish(publish);
+        let message = Event::Data(vec![Packet::Publish(publish)]);
         self.router_tx.send((self.id, message))?;
         Ok(())
     }
@@ -77,7 +77,7 @@ impl LinkTx {
     pub fn subscribe<S: Into<String>>(&mut self, filter: S) -> Result<(), LinkError> {
         let subscribe = Subscribe::new(filter.into(), QoS::AtMostOnce);
         let packet = Packet::Subscribe(subscribe);
-        let message = RouterInMessage::Data(vec![packet]);
+        let message = Event::Data(vec![packet]);
         self.router_tx.send((self.id, message))?;
         Ok(())
     }
@@ -85,9 +85,8 @@ impl LinkTx {
 
 pub struct LinkRx {
     id: usize,
-    router_tx: Sender<(Id, RouterInMessage)>,
-    link_rx: Receiver<RouterOutMessage>,
-    tracker: Tracker,
+    router_tx: Sender<(Id, Event)>,
+    link_rx: Receiver<Notification>,
     max_inflight_requests: usize,
 }
 
@@ -95,64 +94,26 @@ impl LinkRx {
     pub(crate) fn new(
         id: usize,
         max_inflight_requests: usize,
-        router_tx: Sender<(Id, RouterInMessage)>,
-        link_rx: Receiver<RouterOutMessage>,
+        router_tx: Sender<(Id, Event)>,
+        link_rx: Receiver<Notification>,
     ) -> LinkRx {
         LinkRx {
             id,
             max_inflight_requests,
             router_tx,
             link_rx,
-            tracker: Tracker::new(100),
         }
     }
 
-    pub fn recv(&mut self) -> Result<Option<DataReply>, LinkError> {
+    pub fn recv(&mut self) -> Result<Option<Data>, LinkError> {
         let message = self.link_rx.recv()?;
         let message = self.handle_router_response(message);
-
-        // A response from router should trigger new request. Some times
-        // requests. For example, topics reply should trigger next topics
-        // requsest and data request. Not sending a data request is a
-        // potential deadlock until next topics response. So just try_send
-        // is a problem.
-        //
-        // send until inflight requests reaches maximum configured (which is
-        // also the size of response channel to this connection) because router
-        // can keep receiving requests as the below loop aggressively sends
-        // all the possible requests. But requests shouldn't be infinite as
-        // router will eventually fill connection queue with responses and
-        // to finally result if channel full error. Inflight requests should
-        // stop at the capacity of response channel
-        while self.tracker.inflight() < self.max_inflight_requests {
-            // New topic request because of topic response should not fill
-            // inflight queue or else it it a block.
-            match self.tracker.next() {
-                Some(request) => {
-                    trace!("Id = {:14}, Request = {:?}", self.id, request);
-                    self.router_tx.send((self.id, request))?;
-                }
-                None => break,
-            }
-        }
-
         Ok(message)
     }
 
-    fn handle_router_response(&mut self, message: RouterOutMessage) -> Option<DataReply> {
+    fn handle_router_response(&mut self, message: Notification) -> Option<Data> {
         match message {
-            RouterOutMessage::TopicsReply(reply) => {
-                trace!(
-                    "{:11} {:14} Id = {}, Count = {}",
-                    "topics",
-                    "reply",
-                    self.id,
-                    reply.topics.len()
-                );
-                self.tracker.track_new_topics(&reply);
-                None
-            }
-            RouterOutMessage::DataReply(reply) => {
+            Notification::Data(reply) => {
                 trace!(
                     "{:11} {:14} Id = {}, Count = {}",
                     "data",
@@ -160,7 +121,6 @@ impl LinkRx {
                     self.id,
                     reply.payload.len()
                 );
-                self.tracker.update_data_tracker(&reply);
                 Some(reply)
             }
             message => {
