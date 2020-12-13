a32d13918139fe5025e501149cd68c75ad494e1a
Ravi Teja <kraviteza@gmail.com>
2020-12-13 12:27:50 +0530

    Use segments and cleanup some tests (#200)
    Co-authored-by: tekjar <raviteja@bytebeam.io>

--- rumqttlog/src/logs/data.rs
+++ rumqttlog/src/logs/data.rs
@@ -1,14 +1,19 @@
 use std::collections::HashMap;
 use std::io;
 
-use crate::volatile::Log;
 use crate::Config;
 use bytes::Bytes;
 use std::sync::Arc;
+use segments::MemoryLog;
 
 pub(crate) struct DataLog {
     config: Arc<Config>,
-    logs: HashMap<String, Log>,
+    logs: HashMap<String, Data>,
+}
+
+struct Data {
+    retained: Option<(u64, Bytes)>,
+    log: MemoryLog<Bytes>
 }
 
 impl DataLog {
@@ -23,15 +28,18 @@ impl DataLog {
     /// if this topic is new along with the offset of append
     pub fn append(&mut self, topic: &str, record: Bytes) -> io::Result<(bool, (u64, u64))> {
         // Entry instead of if/else?
-        if let Some(log) = self.logs.get_mut(topic) {
-            let offsets = log.append(record);
+        if let Some(data) = self.logs.get_mut(topic) {
+            let offsets = data.log.append(record.len(), record);
             Ok((false, offsets))
         } else {
             let max_segment_size = self.config.max_segment_size;
             let max_segment_count = self.config.max_segment_count;
-            let mut log = Log::new(max_segment_size, max_segment_count);
-            let offsets = log.append(record);
-            self.logs.insert(topic.to_owned(), log);
+            let mut data = Data {
+                retained: None,
+                log: MemoryLog::new(max_segment_size, max_segment_count)
+            };
+            let offsets = data.log.append(record.len(), record);
+            self.logs.insert(topic.to_owned(), data);
             Ok((true, offsets))
         }
     }
@@ -40,26 +48,43 @@ impl DataLog {
     /// if this topic is new along with the offset of append
     pub fn retain(&mut self, topic: &str, record: Bytes) -> io::Result<bool> {
         if let Some(log) = self.logs.get_mut(topic) {
-            log.retain(record);
+            if record.is_empty() {
+                log.retained = None;
+            } else {
+                let retained = log.retained.get_or_insert((0, record.clone()));
+                retained.0 += 1;
+                retained.1 = record;
+            }
+
             Ok(false)
         } else {
             let max_segment_size = self.config.max_segment_size;
             let max_segment_count = self.config.max_segment_count;
-            let mut log = Log::new(max_segment_size, max_segment_count);
+            let mut data = Data {
+                retained: None,
+                log: MemoryLog::new(max_segment_size, max_segment_count)
+            };
 
-            log.retain(record);
-            self.logs.insert(topic.to_owned(), log);
+            if record.is_empty() {
+                data.retained = None;
+            } else {
+                let retained = data.retained.get_or_insert((0, record.clone()));
+                retained.0 += 1;
+                retained.1 = record;
+            }
+
+            self.logs.insert(topic.to_owned(), data);
             Ok(true)
         }
     }
 
     fn next_offset(&self, topic: &str) -> Option<(u64, u64)> {
-        let log = match self.logs.get(topic) {
+        let data = match self.logs.get(topic) {
             Some(log) => log,
             None => return None,
         };
 
-        Some(log.next_offset())
+        Some(data.log.next_offset())
     }
 
     pub fn seek_offsets_to_end(&self, topic: &mut (String, u8, (u64, u64))) {
@@ -77,13 +102,21 @@ impl DataLog {
     ) -> io::Result<Option<(Option<u64>, u64, u64, u64, Vec<Bytes>)>> {
         // Router during data request and notifications will check both
         // native and replica commitlog where this topic doesn't exist
-        let log = match self.logs.get_mut(topic) {
+        let data = match self.logs.get_mut(topic) {
             Some(log) => log,
             None => return Ok(None),
         };
 
-        let (jump, segment, offset, last_retain, data) =
-            log.readv(in_segment, in_offset, last_retain);
+        let (jump, segment, offset, mut out) = data.log.readv(in_segment, in_offset);
+
+        let mut last_retain = last_retain;
+        if let Some((id, publish)) = &mut data.retained {
+            if *id != last_retain {
+                out.push(publish.clone());
+                last_retain = *id;
+            }
+        }
+
 
         // For debugging. Will be removed later
         // println!(
@@ -94,6 +127,6 @@ impl DataLog {
         //     offset,
         //     data.len()
         // );
-        Ok(Some((jump, segment, offset, last_retain, data)))
+        Ok(Some((jump, segment, offset, last_retain, out)))
     }
 }
