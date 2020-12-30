b638ffbaf26330e0f16ac8672ba493421e527e6f
tekjar <raviteja@bytebeam.io>
2020-12-30 17:52:06 +0530

    Fix broker config in example

--- rumqttd/examples/broker.rs
+++ rumqttd/examples/broker.rs
@@ -1,16 +1,10 @@
 use librumqttd::Broker;
-use serde::{Deserialize, Serialize};
 use std::thread;
-
-#[derive(Debug, Default, Serialize, Deserialize, Clone)]
-struct Config {
-    broker: librumqttd::Config,
-}
 
 fn main() {
     pretty_env_logger::init();
-    let config: Config = confy::load_path("config/rumqttd.conf").unwrap();
-    let mut broker = Broker::new(config.broker);
+    let config: librumqttd::Config = confy::load_path("config/rumqttd.conf").unwrap();
+    let mut broker = Broker::new(config);
 
     let mut tx = broker.link("localclient").unwrap();
     thread::spawn(move || {
