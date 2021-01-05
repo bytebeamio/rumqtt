d3d640f32c9601f68a570294df75277c980bface
tekjar <raviteja@bytebeam.io>
2021-01-05 22:20:58 +0530

    Remove special read connect

--- mqttbytes/src/read.rs
+++ mqttbytes/src/read.rs
@@ -2,25 +2,6 @@ use crate::*;
 use bytes::BytesMut;
 use packets::*;
 
-/// Reading connect packet is a separate method as it's used to bootstrap
-/// protocol version of the connection. `read` method uses this information
-/// to parse packets of the resulting version
-pub fn read_connect(stream: &mut BytesMut, max_packet_size: usize) -> Result<Connect, Error> {
-    let fixed_header = check(stream.iter(), max_packet_size)?;
-
-    // Test with a stream with exactly the size to check border panics
-    let packet = stream.split_to(fixed_header.frame_length());
-    let packet_type = fixed_header.packet_type()?;
-
-    let packet = packet.freeze();
-    let connect = match packet_type {
-        PacketType::Connect => Connect::read(fixed_header, packet)?,
-        v => return Err(Error::NotConnect(v)),
-    };
-
-    Ok(connect)
-}
-
 /// Reads a stream of bytes and extracts next MQTT packet out of it
 pub fn read(stream: &mut BytesMut, max_size: usize) -> Result<Packet, Error> {
     let fixed_header = check(stream.iter(), max_size)?;
@@ -41,7 +22,7 @@ pub fn read(stream: &mut BytesMut, max_size: usize) -> Result<Packet, Error> {
 
     let packet = packet.freeze();
     let packet = match packet_type {
-        PacketType::Connect => return Err(Error::UnexpectedConnect),
+        PacketType::Connect => Packet::Connect(Connect::read(fixed_header, packet)?),
         PacketType::ConnAck => Packet::ConnAck(ConnAck::read(fixed_header, packet)?),
         PacketType::Publish => Packet::Publish(Publish::read(fixed_header, packet)?),
         PacketType::PubAck => Packet::PubAck(PubAck::read(fixed_header, packet)?),
