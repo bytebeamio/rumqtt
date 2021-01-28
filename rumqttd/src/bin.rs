1b3cce53eecdd5269ef4e2cf02f18946fcf9d2f0
qm3ster <qm3ster@gmail.com>
2021-01-28 14:37:02 +0200

    Make profiler a feature, not target.cfg

--- rumqttd/src/bin.rs
+++ rumqttd/src/bin.rs
@@ -16,6 +16,7 @@ struct CommandLine {
         default = "PathBuf::from(\"config/rumqttd.conf\")"
     )]
     config: PathBuf,
+    #[cfg(feature = "prof")]
     /// enable profiling
     #[argh(switch, short = 'p')]
     profile: bool,
@@ -26,33 +27,34 @@ fn main() {
     let commandline: CommandLine = argh::from_env();
     let config: Config = confy::load_path(&commandline.config).unwrap();
 
-    #[cfg(not(target_env = "msvc"))]
-    let guard = if commandline.profile {
-        Some(pprof::ProfilerGuard::new(100).unwrap())
-    } else {
-        None
-    };
+    #[cfg(feature = "prof")]
+    let _guard = prof::new(commandline.profile);
 
     let o = Broker::new(config).start();
     println!("Stopping broker!! Error = {:?}", o);
-
-    #[cfg(not(target_env = "msvc"))]
-    if commandline.profile {
-        profile("rumqttd.pb", &guard.unwrap());
-    }
 }
 
-#[cfg(not(target_env = "msvc"))]
-fn profile(name: &str, guard: &pprof::ProfilerGuard) {
+#[cfg(feature = "prof")]
+mod prof {
     use pprof::protos::Message;
-    use std::{fs::File, io::Write as _};
-
-    if let Ok(report) = guard.report().build() {
-        let mut file = File::create(name).unwrap();
-        let profile = report.pprof().unwrap();
-
-        let mut content = Vec::new();
-        profile.encode(&mut content).unwrap();
-        file.write_all(&content).unwrap();
-    };
+    pub struct Profile(pprof::ProfilerGuard<'static>);
+    pub fn new(enable: bool) -> Option<Profile> {
+        if enable{
+            Some(Profile(pprof::ProfilerGuard::new(100).unwrap()))
+        } else {
+            None
+        }
+    }
+    impl Drop for Profile {
+        fn drop(&mut self) {
+            if let Ok(report) = self.0.report().build() {
+                let profile = report.pprof().unwrap();
+                let mut buf = Vec::with_capacity(profile.encoded_len());
+                profile.encode(&mut buf).unwrap();
+                let path = "rumqttd.pb";
+                std::fs::write(path, &buf).unwrap();
+                println!("wrote profile to {}", path);
+            }
+        }
+    }
 }
