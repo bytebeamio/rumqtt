6fbfcaa495f405b96b10596c69bb5d51e369d98e
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2021-10-30 20:13:34 +0200

    Add unix feature (#317)
    * add unix feature
    
    * use #[cfg(unix)]

--- rumqttc/src/lib.rs
+++ rumqttc/src/lib.rs
@@ -194,6 +194,7 @@ impl From<Unsubscribe> for Request {
 pub enum Transport {
     Tcp,
     Tls(TlsConfiguration),
+    #[cfg(unix)]
     Unix,
     #[cfg(feature = "websocket")]
     #[cfg_attr(docsrs, doc(cfg(feature = "websocket")))]
@@ -234,6 +235,7 @@ impl Transport {
         Self::Tls(tls_config)
     }
 
+    #[cfg(unix)]
     pub fn unix() -> Self {
         Self::Unix
     }
