411cfa5b3a34e14463c3609b8849473e5d14c9a4
Devdutt Shenoi <devdutt@bytebeam.io>
2022-03-07 22:32:37 +0530

    Make example easier to understand

--- rumqttc/examples/websocket.rs
+++ rumqttc/examples/websocket.rs
@@ -1,68 +1,59 @@
-use std::error::Error;
+#[cfg(feature = "websocket")]
+use rumqttc::{self, AsyncClient, MqttOptions, QoS, Transport};
+#[cfg(feature = "websocket")]
+use std::{error::Error, time::Duration};
+#[cfg(feature = "websocket")]
+use tokio::{task, time};
 
 #[cfg(feature = "websocket")]
-mod example {
-    use super::*;
-    use rumqttc::{self, AsyncClient, MqttOptions, QoS, Transport};
-    use std::time::Duration;
-    use tokio::{task, time};
+#[tokio::main(worker_threads = 1)]
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
 
-    pub async fn run() -> Result<(), Box<dyn Error>> {
-        pretty_env_logger::init();
+    // port parameter is ignored when scheme is websocket
+    let mut mqttoptions = MqttOptions::new(
+        "clientId-aSziq39Bp3",
+        "ws://broker.mqttdashboard.com:8000/mqtt",
+        8000,
+    );
+    mqttoptions.set_transport(Transport::Ws);
+    mqttoptions.set_keep_alive(Duration::from_secs(60));
 
-        // port parameter is ignored when scheme is websocket
-        let mut mqttoptions = MqttOptions::new(
-            "clientId-aSziq39Bp3",
-            "ws://broker.mqttdashboard.com:8000/mqtt",
-            8000,
-        );
-        mqttoptions.set_transport(Transport::Ws);
-        mqttoptions.set_keep_alive(Duration::from_secs(60));
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    task::spawn(async move {
+        requests(client).await;
+        time::sleep(Duration::from_secs(3)).await;
+    });
 
-        let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-        task::spawn(async move {
-            requests(client).await;
-            time::sleep(Duration::from_secs(3)).await;
-        });
-
-        loop {
-            let event = eventloop.poll().await;
-            match event {
-                Ok(ev) => println!("{:?}", ev),
-                Err(err) => println!("{:?}", err),
-            }
+    loop {
+        let event = eventloop.poll().await;
+        match event {
+            Ok(ev) => println!("{:?}", ev),
+            Err(err) => println!("{:?}", err),
         }
     }
+}
 
-    async fn requests(client: AsyncClient) {
+#[cfg(feature = "websocket")]
+async fn requests(client: AsyncClient) {
+    client
+        .subscribe("hello/world", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    for i in 1..=10 {
         client
-            .subscribe("hello/world", QoS::AtMostOnce)
+            .publish("hello/world", QoS::ExactlyOnce, false, vec![1; i])
             .await
             .unwrap();
 
-        for i in 1..=10 {
-            client
-                .publish("hello/world", QoS::ExactlyOnce, false, vec![1; i])
-                .await
-                .unwrap();
-
-            time::sleep(Duration::from_secs(1)).await;
-        }
-
-        time::sleep(Duration::from_secs(120)).await;
+        time::sleep(Duration::from_secs(1)).await;
     }
+
+    time::sleep(Duration::from_secs(120)).await;
 }
 
 #[cfg(not(feature = "websocket"))]
-mod example {
-    use super::*;
-
-    pub async fn run() -> Result<(), Box<dyn Error>> {
-        panic!("Enable websocket feature with `--features=websocket`");
-    }
-}
-
-#[tokio::main(worker_threads = 1)]
-async fn main() -> Result<(), Box<dyn Error>> {
-    example::run().await
+fn main() {
+    panic!("Enable websocket feature with `--features=websocket`");
 }
