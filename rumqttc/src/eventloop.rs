7d3fa97e375d14eea26a3d88c2db7595a6fbeabb
Devdutt Shenoi <devdutt@bytebeam.io>
2022-09-25 00:33:05 +0530

    Do not require `MqttOptions` for `tls_connect()`

--- rumqttc/src/eventloop.rs
+++ rumqttc/src/eventloop.rs
@@ -241,7 +241,7 @@ async fn network_connect(options: &MqttOptions) -> Result<Network, ConnectionErr
         }
         #[cfg(feature = "use-rustls")]
         Transport::Tls(tls_config) => {
-            let socket = tls::tls_connect(options, &tls_config).await?;
+            let socket = tls::tls_connect(&options.broker_addr, options.port, &tls_config).await?;
             Network::new(socket, options.max_incoming_packet_size)
         }
         #[cfg(unix)]
