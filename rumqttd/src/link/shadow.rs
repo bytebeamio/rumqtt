c81a702c736cf663a580464fb9c75bca6b0dc531
Devdutt Shenoi <devdutt@outlook.in>
2022-10-06 11:12:11 +0530

    fix: adapt to breaking change in shadow link

--- rumqttd/src/link/shadow.rs
+++ rumqttd/src/link/shadow.rs
@@ -72,7 +72,6 @@ impl ShadowLink {
         let client_id = connect.client_id.clone();
 
         let (link_tx, link_rx, _ack) = Link::new(
-            None,
             &client_id,
             router_tx,
             true,
