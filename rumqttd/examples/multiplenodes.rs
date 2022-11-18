2504b84a355bcd4f880ead45eb543379aa6da5d2
swanandx <73115739+swanandx@users.noreply.github.com>
2022-11-18 15:46:26 +0530

    feat: one subscriber, many publishers

--- rumqttd/examples/multiplenodes.rs
+++ rumqttd/examples/multiplenodes.rs
@@ -1,5 +1,3 @@
-use std::{thread, time::Duration};
-
 use rumqttd::Broker;
 
 fn main() {
@@ -7,38 +5,42 @@ fn main() {
     tracing_subscriber::fmt::init();
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     let config = config::Config::builder()
-        .add_source(config::File::with_name(&format!("{manifest_dir}/demo.toml")))
+        .add_source(config::File::with_name(&format!(
+            "{manifest_dir}/demo.toml"
+        )))
         .build()
         .unwrap(); // Config::default() doesn't have working values
 
     let config = config.try_deserialize().unwrap();
-    println!("{config:?}");
     let broker = Broker::new(config);
 
     const CONNECTIONS: usize = 10;
-    const MESSAGES_PER_CONNECTION: usize = 2;
 
-    let mut rxs = Vec::with_capacity(CONNECTIONS);
+    let (mut link_tx, mut link_rx) = broker
+        .link("the_subscriber")
+        .expect("New link should be made");
+    link_tx
+        .subscribe("hello/+/world")
+        .expect("link should subscribe");
+
+    link_rx.recv().expect("Should recieve Ack");
+
     for i in 0..CONNECTIONS {
         let client_id = format!("client_{i}");
-        let (mut link_tx, link_rx) = broker.link(&client_id).expect("New link should be made");
-        if i.is_power_of_two() { // 0, 1, 2, 4, 8 .. just to make some subscriber clients
-            link_tx.subscribe("hello/world").unwrap();
-            rxs.push(link_rx);
-        } else {
-            thread::spawn(move || {
-                thread::sleep(Duration::from_secs(1));
-                for _ in 0..MESSAGES_PER_CONNECTION {
-                    link_tx.publish("hello/world", vec![1, 2, 3]).unwrap();
-                }
-            });
-        }
+        let topic = format!("hello/{}/world", client_id);
+        let payload = vec![0u8; 1_000]; // 0u8 is one byte, so total ~1KB
+        let (mut link_tx, _link_rx) = broker.link(&client_id).expect("New link should be made");
+        link_tx.publish(topic, payload).unwrap();
     }
 
+    let mut count = 0;
     loop {
-        for rx in &mut rxs {
-            let not = rx.recv().unwrap();
-            println!("RECV {not:?}");
+        let notification = link_rx.recv().unwrap();
+        if notification.is_some() {
+            count += 1;
         }
+        // do we need to print what data we got?
+        println!("RECV {notification:?}");
+        dbg!(count);
     }
 }
