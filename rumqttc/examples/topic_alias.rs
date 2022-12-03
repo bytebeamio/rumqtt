3d06747b87360df30f82a273f81ff8f665c90859
swanandx <73115739+swanandx@users.noreply.github.com>
2022-12-04 00:30:10 +0530

    feat: example to show new API with topic alias

---
+++ rumqttc/examples/topic_alias.rs
@@ -0,0 +1,72 @@
+use rumqttc::v5::mqttbytes::QoS;
+use tokio::{task, time};
+
+use rumqttc::v5::{AsyncClient, MqttOptions};
+use std::error::Error;
+use std::time::Duration;
+
+#[tokio::main(worker_threads = 1)]
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+
+    let mut mqttoptions = MqttOptions::new("test-1", "broker.emqx.io", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    task::spawn(async move {
+        requests(client).await;
+        time::sleep(Duration::from_secs(3)).await;
+    });
+
+    while let Ok(event) = eventloop.poll().await {
+        println!("{:?}", event);
+    }
+
+    Ok(())
+}
+
+async fn requests(client: AsyncClient) {
+    client
+        .subscribe("hello/world", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    client
+        .subscribe("bye/world", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    let mut publisher1 = client.publisher("hello/world");
+    publisher1.topic_alias(3);
+
+    publisher1
+        .publish(QoS::ExactlyOnce, false, vec![3; 3])
+        .await
+        .unwrap();
+
+    let mut publisher2 = client.publisher("bye/world");
+    publisher2.topic_alias(5);
+
+    publisher2
+        .publish(QoS::ExactlyOnce, false, vec![3; 3])
+        .await
+        .unwrap();
+
+    for i in 1..=10 {
+        // just for choosing publisher alternately
+        let publisher = if i % 2 == 0 {
+            &mut publisher1
+        } else {
+            &mut publisher2
+        };
+
+        publisher
+            .publish(QoS::ExactlyOnce, false, vec![1; i])
+            .await
+            .unwrap();
+
+        time::sleep(Duration::from_secs(1)).await;
+    }
+
+    time::sleep(Duration::from_secs(120)).await;
+}
