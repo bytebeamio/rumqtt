591015cc0746fcad5ccdc1f863be45290f064f9e
swanandx <73115739+swanandx@users.noreply.github.com>
2022-12-30 13:01:31 +0530

    feat: register all publish and subscribes before poll

--- rumqttc/examples/asyncpubsub_v5.rs
+++ rumqttc/examples/asyncpubsub_v5.rs
@@ -1,10 +1,7 @@
 use rumqttc::v5::mqttbytes::QoS;
-use tokio::sync::Mutex;
-use tokio::{task, time};
-
 use rumqttc::v5::{unsync::EventLoop, MqttOptions};
 use std::error::Error;
-use std::sync::Arc;
+
 use std::time::Duration;
 
 #[tokio::main(worker_threads = 1)]
@@ -15,15 +12,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut mqttoptions = MqttOptions::new("test-1", "localhost", 1884);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
-    let eventloop = Arc::new(Mutex::new(EventLoop::new(mqttoptions, 10)));
-    let cloned = eventloop.clone();
-    task::spawn(async move {
-        requests(cloned).await;
-        time::sleep(Duration::from_secs(3)).await;
-    });
+    let mut eventloop = EventLoop::new(mqttoptions, 10);
+
+    eventloop
+        .subscribe("hello/world", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    for i in 1..=10 {
+        eventloop
+            .publish("hello/world", QoS::AtMostOnce, false, vec![1; i])
+            .await
+            .unwrap();
+    }
 
     loop {
-        let event = eventloop.lock().await.poll().await;
+        let event = eventloop.poll().await;
         match &event {
             Ok(v) => {
                 println!("Event = {:?}", v);
@@ -34,27 +38,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-}
-
-async fn requests(eventloop: Arc<Mutex<EventLoop>>) {
-    // let mut eventloop = eventloop.lock().await;
-    eventloop
-        .lock()
-        .await
-        .subscribe("hello/world", QoS::AtMostOnce)
-        .await
-        .unwrap();
-
-    for i in 1..=10 {
-        eventloop
-            .lock()
-            .await
-            .publish("hello/world", QoS::AtMostOnce, false, vec![1; i])
-            .await
-            .unwrap();
-
-        time::sleep(Duration::from_secs(1)).await;
-    }
-
-    // time::sleep(Duration::from_secs(120)).await;
 }
