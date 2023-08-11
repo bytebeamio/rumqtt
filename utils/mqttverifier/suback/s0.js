c47620f6a4ee4d73a3aaefed4bba476833de5a01
Carlo Corradini <carlo.ita98@gmail.com>
2023-08-11 19:55:07 +0200

    chore(mqttverifier): fix typo and better structure with jsdoc type (#679)

--- utils/mqttverifier/suback/s0.js
+++ utils/mqttverifier/suback/s0.js
@@ -1,31 +1,23 @@
-const mqtt = require('mqtt-packet');
+import mqtt from "mqtt-packet";
+import { printer } from "../printer.js";
 
+/** @type {mqtt.Packet} */
 const packet = {
-	cmd: 'suback',
-	messageId: 42,
-	properties: { // MQTT 5.0 properties
-		reasonString: 'test',
-		userProperties: {
-			'test': 'test'
-		}
-	},
-	granted: [0, 1, 2, 128]
-}
+    cmd: "suback",
+    messageId: 42,
+    properties: {
+        // MQTT 5.0 properties
+        reasonString: "test",
+        userProperties: {
+            test: "test"
+        }
+    },
+    granted: [0, 1, 2, 128]
+};
 
-
+/** @type {unknown} */
 const opts = { protocolVersion: 5 };
-let data = mqtt.generate(packet, opts);
-print(data);
 
+const buffer = mqtt.generate(packet, opts);
 
-function print(data) {
-	let out = "";
-	for (var i = 0; i < data.length; i++) {
-		const hex = Number(data[i]).toString(16).padStart(2, '0');
-		out = out + "0x" + hex + ", ";
-	}
-
-	console.log("[" + out + "]");
-}
-
-
+printer(buffer);
