e626d6c91824674f84d0ee74da0f33b7b81fe07b
swanandx <73115739+swanandx@users.noreply.github.com>
2023-12-02 14:23:35 +0530

    fix: ping

--- rumqttc/src/v5/mqttbytes/v5/ping.rs
+++ rumqttc/src/v5/mqttbytes/v5/ping.rs
@@ -5,7 +5,7 @@ pub struct PingReq;
 
 impl PingReq {
     pub fn write(payload: &mut Vec<u8>) -> Result<usize, Error> {
-        payload.put_slice(&[0xC0, 0x00]);
+        payload.extend_from_slice(&[0xC0, 0x00]);
         Ok(2)
     }
 }
@@ -15,7 +15,7 @@ pub struct PingResp;
 
 impl PingResp {
     pub fn write(payload: &mut Vec<u8>) -> Result<usize, Error> {
-        payload.put_slice(&[0xD0, 0x00]);
+        payload.extend_from_slice(&[0xD0, 0x00]);
         Ok(2)
     }
 }
