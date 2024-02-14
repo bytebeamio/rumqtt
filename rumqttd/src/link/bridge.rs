d34871065ca9cae7830a67b86aedfefcb46af960
Andrew Walbran <qwandor@google.com>
2024-02-14 16:45:17 +0000

    chore(rumqttd): update rustls and friends (#795)

--- rumqttd/src/link/bridge.rs
+++ rumqttd/src/link/bridge.rs
@@ -18,8 +18,8 @@ use tokio::{
 #[cfg(feature = "use-rustls")]
 use tokio_rustls::{
     rustls::{
-        client::InvalidDnsNameError, Certificate, ClientConfig, Error as TLSError,
-        OwnedTrustAnchor, PrivateKey, RootCertStore, ServerName,
+        pki_types::{InvalidDnsNameError, ServerName},
+        ClientConfig, Error as TLSError, RootCertStore,
     },
     TlsConnector,
 };
@@ -198,62 +198,39 @@ pub async fn tls_connect<P: AsRef<Path>>(
 ) -> Result<Box<dyn N>, BridgeError> {
     let mut root_cert_store = RootCertStore::empty();
 
-    let ca_certs = rustls_pemfile::certs(&mut BufReader::new(Cursor::new(fs::read(ca_file)?)))?;
-    let trust_anchors = ca_certs.iter().map_while(|cert| {
-        if let Ok(ta) = webpki::TrustAnchor::try_from_cert_der(&cert[..]) {
-            Some(OwnedTrustAnchor::from_subject_spki_name_constraints(
-                ta.subject,
-                ta.spki,
-                ta.name_constraints,
-            ))
-        } else {
-            None
-        }
-    });
-
-    root_cert_store.add_trust_anchors(trust_anchors);
+    for cert in rustls_pemfile::certs(&mut BufReader::new(Cursor::new(fs::read(ca_file)?))) {
+        root_cert_store.add(cert?)?;
+    }
 
     if root_cert_store.is_empty() {
         return Err(BridgeError::NoValidCertInChain);
     }
 
-    let config = ClientConfig::builder()
-        .with_safe_defaults()
-        .with_root_certificates(root_cert_store);
+    let config = ClientConfig::builder().with_root_certificates(root_cert_store);
 
     let config = if let Some(ClientAuth {
         certs: certs_path,
         key: key_path,
     }) = client_auth_opt
     {
-        let read_certs =
-            rustls_pemfile::certs(&mut BufReader::new(Cursor::new(fs::read(certs_path)?)))?;
+        let certs = rustls_pemfile::certs(&mut BufReader::new(Cursor::new(fs::read(certs_path)?)))
+            .collect::<Result<Vec<_>, _>>()?;
 
-        let read_keys = match rustls_pemfile::read_one(&mut BufReader::new(Cursor::new(fs::read(
+        let key = match rustls_pemfile::read_one(&mut BufReader::new(Cursor::new(fs::read(
             key_path,
         )?)))? {
-            Some(rustls_pemfile::Item::RSAKey(_)) => rustls_pemfile::rsa_private_keys(
-                &mut BufReader::new(Cursor::new(fs::read(key_path)?)),
-            )?,
-            Some(rustls_pemfile::Item::PKCS8Key(_)) => rustls_pemfile::pkcs8_private_keys(
-                &mut BufReader::new(Cursor::new(fs::read(key_path)?)),
-            )?,
+            Some(rustls_pemfile::Item::Pkcs1Key(key)) => key.into(),
+            Some(rustls_pemfile::Item::Pkcs8Key(key)) => key.into(),
             None | Some(_) => return Err(BridgeError::NoValidCertInChain),
         };
 
-        let read_key = match read_keys.first() {
-            Some(v) => v.clone(),
-            None => return Err(BridgeError::NoValidCertInChain),
-        };
-
-        let certs = read_certs.into_iter().map(Certificate).collect();
-        config.with_client_auth_cert(certs, PrivateKey(read_key))?
+        config.with_client_auth_cert(certs, key)?
     } else {
         config.with_no_client_auth()
     };
 
     let connector = TlsConnector::from(Arc::new(config));
-    let domain = ServerName::try_from(host).unwrap();
+    let domain = ServerName::try_from(host)?.to_owned();
     Ok(Box::new(connector.connect(domain, tcp).await?))
 }
 
