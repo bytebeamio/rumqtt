0e5d53a69b324abe47e7a927ffc22f046159ed0c
tinzhu <tinzhu@microsoft.com>
2024-04-17 17:16:51 +0800

    Test re-authentication.

--- rumqttc/examples/auth.rs
+++ rumqttc/examples/auth.rs
@@ -1,5 +1,5 @@
 
-use rumqttc::v5::mqttbytes::QoS;
+use rumqttc::v5::mqttbytes::{QoS, v5::AuthReasonCode, v5::AuthProperties, v5::Auth};
 use rumqttc::v5::{AsyncClient, MqttOptions};
 use tokio::task;
 use std::error::Error;
@@ -18,6 +18,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     mqttoptions.set_connection_timeout(20);
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
 
+    let scram2 = ScramClient::new("user1", "123456", None);
+    let (scram2, client_first2) = scram2.client_first();
+
     task::spawn(async move {
         let server_first: String = client.recv_server_auth_data().await.unwrap();
         let scram = scram.handle_server_first(&server_first).unwrap();
@@ -26,6 +29,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         client.subscribe("rumqtt_auth/topic", QoS::AtLeastOnce).await.unwrap();
         client.publish("rumqtt_auth/topic", QoS::AtLeastOnce, false, "hello world").await.unwrap();
+
+        // Reauthentication
+        let props = AuthProperties {
+            authentication_method: Some("SCRAM-SHA-256".to_string()),
+            authentication_data: Some(client_first2.clone().into()),
+            reason_string: None,
+            user_properties: vec![],
+        };
+
+        client.auth(AuthReasonCode::Reauthenticate, Some(props)).await.unwrap();
+
+        let server_first: String = client.recv_server_auth_data().await.unwrap();
+        let scram2 = scram2.handle_server_first(&server_first).unwrap();
+        let (scram2, client_final2) = scram2.client_final();
+        client.send_client_auth_data(client_final2).await.unwrap();
     });
 
     loop {
