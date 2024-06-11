5f50d6cd23aee0da70ee68ccaa4cf6585189d627
Devdutt Shenoi <devdutt@bytebeam.io>
2024-06-11 11:42:38 +0530

    server struct exports

--- rumqttd/src/lib.rs
+++ rumqttd/src/lib.rs
@@ -23,7 +23,7 @@ pub use link::local;
 pub use link::meters;
 pub use router::{Alert, Forward, IncomingMeter, Meter, Notification, OutgoingMeter};
 use segments::Storage;
-pub use server::Broker;
+pub use server::{Broker, Error};
 
 pub use self::router::shared_subs::Strategy;
 
