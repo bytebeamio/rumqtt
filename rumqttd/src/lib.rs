163baef47be01c2efe76805586f331906e7bbd76
Marvin Drescher <mdr@ssv-embedded.de>
2024-06-20 11:53:06 +0200

    feat(rumqttd/broker): implemented filter for publish packets
    BREAKING CHANGE: `Router::new` now takes an list
    of `PublishFilterRef` as additional parameter

--- rumqttd/src/lib.rs
+++ rumqttd/src/lib.rs
@@ -21,7 +21,7 @@ use tracing_subscriber::{
 pub use link::alerts;
 pub use link::local;
 pub use link::meters;
-pub use router::{Alert, Forward, IncomingMeter, Meter, Notification, OutgoingMeter, Router};
+pub use router::{Alert, Forward, IncomingMeter, Meter, Notification, OutgoingMeter, PublishFilter, Router, PublishFilterRef};
 use segments::Storage;
 pub use server::{Broker, LinkType, Server};
 
