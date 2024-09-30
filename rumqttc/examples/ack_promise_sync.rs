2632ab11e986b038832445f6dc561a765b86983e
Devdutt Shenoi <devdutt@bytebeam.io>
2024-09-30 16:38:46 +0530

    doc: add examples of ack notify

---
+++ rumqttc/examples/ack_promise_sync.rs
@@ -0,0 +1,92 @@
+use flume::bounded;
+use rumqttc::{Client, MqttOptions, QoS};
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    // color_backtrace::install();
+
+    let mut mqttoptions = MqttOptions::new("test-1", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (client, mut conn) = Client::new(mqttoptions, 10);
+    thread::spawn(move || {
+        for event in conn.iter() {
+            match &event {
+                Ok(v) => {
+                    println!("Event = {v:?}");
+                }
+                Err(e) => {
+                    println!("Error = {e:?}");
+                }
+            }
+        }
+    });
+
+    // Subscribe and wait for broker acknowledgement
+    client
+        .subscribe("hello/world", QoS::AtMostOnce)
+        .unwrap()
+        .blocking_recv()
+        .unwrap();
+    println!("Acknowledged Subscribe");
+
+    // Publish at all QoS levels and wait for broker acknowledgement
+    client
+        .publish("hello/world", QoS::AtMostOnce, false, vec![1; 1])
+        .unwrap()
+        .blocking_recv()
+        .unwrap();
+    println!("Acknowledged Pub(1)");
+
+    client
+        .publish("hello/world", QoS::AtLeastOnce, false, vec![1; 2])
+        .unwrap()
+        .blocking_recv()
+        .unwrap();
+    println!("Acknowledged Pub(2)");
+
+    client
+        .publish("hello/world", QoS::ExactlyOnce, false, vec![1; 3])
+        .unwrap()
+        .blocking_recv()
+        .unwrap();
+    println!("Acknowledged Pub(3)");
+
+    // Spawn threads for each publish, use channel to notify result
+    let (tx, rx) = bounded(1);
+
+    let future = client
+        .publish("hello/world", QoS::AtMostOnce, false, vec![1; 1])
+        .unwrap();
+    let tx_clone = tx.clone();
+    thread::spawn(move || {
+        let res = future.blocking_recv().map(|_| 1);
+        tx_clone.send(res).unwrap()
+    });
+
+    let future = client
+        .publish("hello/world", QoS::AtLeastOnce, false, vec![1; 2])
+        .unwrap();
+    let tx_clone = tx.clone();
+    thread::spawn(move || {
+        let res = future.blocking_recv().map(|_| 2);
+        tx_clone.send(res).unwrap()
+    });
+
+    let future = client
+        .publish("hello/world", QoS::ExactlyOnce, false, vec![1; 3])
+        .unwrap();
+    thread::spawn(move || {
+        let res = future.blocking_recv().map(|_| 3);
+        tx.send(res).unwrap()
+    });
+
+    while let Ok(res) = rx.recv() {
+        println!("Acknowledged = {:?}", res?);
+    }
+
+    Ok(())
+}
