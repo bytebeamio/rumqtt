34bf57186c6083ea1b1015a17e8285c0919160c9
tekjar <raviteja@bytebeam.io>
2024-11-02 20:49:20 +0530

    feat: implement size calculation for packet types and improve event loop structure

--- rumqttc-ng/src/eventloop.rs
+++ rumqttc-ng/src/eventloop.rs
@@ -1,7 +1,13 @@
-use std::{thread, time::Duration};
+use std::{collections::VecDeque, thread, time::Duration};
 
+use base::{messages::Packet, pipe, EventError, EventsRx, XchgPipeA, XchgPipeB};
+
+use crate::Event;
 
 pub struct EventLoopSettings {
+    pub max_clients: usize,
+    pub max_read_buf_size: usize,
+    pub max_write_buf_size: usize,
     pub max_subscriptions: usize,
     pub max_subscription_log_size: usize,
     pub max_inflight_messages: usize,
@@ -9,17 +15,31 @@ pub struct EventLoopSettings {
 
 pub struct EventLoop {
     settings: EventLoopSettings,
+    tx_pool: VecDeque<XchgPipeA<Packet>>,
+    rxs: Vec<XchgPipeB<Packet>>,
+    events: EventsRx<Event>,
 }
 
 impl EventLoop {
     pub fn new(settings: EventLoopSettings) -> Self {
-        EventLoop { settings }
+        let mut tx_pool = VecDeque::new();
+        let mut rxs = Vec::new();
+        let events = EventsRx::new(settings.max_clients);
+
+        for id in 0..settings.max_clients {
+            let (a, b) = pipe(id, settings.max_read_buf_size, settings.max_write_buf_size);
+            tx_pool.push_back(a);
+
+            assert_eq!(rxs.len(), id);
+            rxs.push(b);
+        }
+
+        EventLoop { settings, tx_pool, rxs, events }
     }
 
-    pub fn start(&self) {
+    pub fn start(&mut self) -> Result<(), Error> {
         loop {
-            thread::sleep(Duration::from_millis(100));
-            println!("EventLoop::start");
+            let event = self.events.poll()?;
         }
     }
 }
@@ -30,7 +50,15 @@ impl Default for EventLoopSettings {
             max_subscriptions: 10,
             max_subscription_log_size: 100 * 1024 * 1024,
             max_inflight_messages: 100,
+            max_clients: 10,
+            max_read_buf_size: 1024 * 1024,
+            max_write_buf_size: 1024 * 1024,
         }
     }
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Event error")]
+    EventError(#[from] EventError),
+}
